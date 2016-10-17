0b2cf3d5f5c41516ec8f1572e7b29971af81ca7a
Michael Housh <mhoush@houshhomeenergy.com>
2016-10-17 08:51:18 -0400

    Fixed broken version in __init__.py

--- phonevalidator/__init__.py
+++ phonevalidator/__init__.py
@@ -5,5 +5,5 @@ from .phonevalidator import ValidatorMixin, Validator
 
 __author__ = 'Michael Housh'
 __email__ = 'mhoush@houshhomeenergy.com'
-__version__ = '__version__ = '1.1.0''
+__version__ = '1.1.1'
 __all__ = ['Validator', 'ValidatorMixin']
